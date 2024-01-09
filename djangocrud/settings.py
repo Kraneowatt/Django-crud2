ee786cadd601ed9968a88874ca04de592951f325
hpomen <1@gmail.com>
2024-01-09 12:26:28 -0300

    change in settings god dammit2

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,10 +83,11 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-                default="postgresql://localhost",
+    'default': { dj_database_url.config(
+                default="postgresql://postgres:postgres@localhost/postgres",
                 conn_max_age=600
     )
+    }
 }
 
 
@@ -125,11 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'staticfiles'),
-    )
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL= "/signin"
 
