b698b71a7f846e7167a6673bfd8551cdec13f6a1
hpomen <1@gmail.com>
2024-01-09 11:34:20 -0300

    commented database

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,11 +83,10 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': { dj_database_url.config(
+    'default': dj_database_url.config(
                 default="postgresql://postgres:postgres@localhost/postgres",
                 conn_max_age=600
     )
-    }
 }
 
 
